c1dc7788bcd61cce531c8d7720906ecc1a86aab2
Vlad Emelianov <vlad.emelianov.nz@gmail.com>
2024-12-05 21:21:05 +0300

    Session is inherited from Boto3Session

--- test.py
+++ test.py
@@ -4,6 +4,11 @@ Copyright 2024 Vlad Emelianov
 
 from aioboto3.session import Session
 
-session = Session("aws_access_key_id")
-session = Session(None)
-session = Session(123)
+
+async def main() -> None:
+    session = Session("aws_access_key_id")
+    session = Session(None)
+    session = Session(123)
+
+    async with session.client("s3") as client:
+        client.create_bucket(Bucket="bucket")
